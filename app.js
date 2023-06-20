d5d9e60c64800d17fa6b4c56675fc52d9791de44
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-06-20 16:26:25 +0530

    setup latout styles

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ const app = express();
 
 app.use(expressLayout); //use express-ejs-layouts.
 
-app.use("/", require("./routes/index"));//here provide the routes.
+app.use("/", require("./routes/index")); //here provide the routes.
 
+app.set("layout extractStyles", true); //SETUP THE LAYOUT STYLES
 app.set("view engine", "ejs"); // SETTING UP THE VIEW ENGINE :
 app.set("views", path.join(__dirname, "views"), "views"); //JOIN THE PATH TO THE VIEWS TO ACCESS IT:
+
+app.use(express.static("./assets")); //  WE CAN ACCESS THE ASSETS FOLDER.
 
 app.listen(port, (err) => {
   if (err) {
